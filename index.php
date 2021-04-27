51c8ca107ec505a2b5f4b1d500c69fa8273c27bb
MitaLesa12 <mita.lesa228@gmail.com>
2021-04-27 14:14:01 +0400

    2

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 include_once "class/Engine.php"; //Подключаем класс-движка
 $engine = new Engine(); //Создаем объект класса Engine
 include_once "templates/header.php"; //Подключаем шапку сайта
-if ($engine->getError()) { //Если возникли ошибки, выводим сообщение на
-экран
+if ($engine->getError()) { //Если возникли ошибки, выводим сообщение на экран
 echo "<div style='border:1px solid red;padding:10px;margin: 10px auto;
 width: 500px;'>" . $engine->getError() . "</div>";
 }
